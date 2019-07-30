5bfe05f06ce72a08cf9b42e740c9b0c2853e2397
ParrySMS <yh@szu.me>
2019-07-30 19:41:30 +0800

    modified: add class demo in testStruc.cs

--- testStruc.cs
+++ testStruc.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Text;
 
-struct Books
+//struct Books
+class Books
 {
     private string title;
     private string author;
     private string subject;
     private int book_id;
+
+    public Books()
+    {
+        Console.WriteLine("default Books");
+    }
+    public Books(string t, string a, string s, int id)
+    {
+        Console.WriteLine("params Books");
+        title = t;
+        author = a;
+        subject = s;
+        book_id = id;
+    }
     public void getValues(string t, string a, string s, int id)
     {
         title = t;
@@ -24,17 +38,55 @@ struct Books
 
 };
 
+class Shape
+{
+    protected int width;
+    protected int height;
+    public Shape()
+    {
+        Console.WriteLine("default Shape");
+    }
+    public Shape(int w, int h)
+    {
+        Console.WriteLine("params Shape");
+        width = w;
+        height = h;
+    }
+
+    public void setWidth(int w)
+    {
+        width = w;
+    }
+    public void setHeight(int h)
+    {
+        height = h;
+    }
+    
+};
+
+class ThreeKing : Books,Shape
+{
+    public ThreeKing(string t, string a, string s, int id) {
+        Books(t,a,s,id);
+        Shape(id, id);
+        Console.WriteLine("ThreeKing Books");
+    }
+
+};
+
 public class testStructure
 {
     public static void Main(string[] args)
     {
 
         Books Book1 = new Books(); /* 声明 Book1，类型为 Book */
-        Books Book2 = new Books(); /* 声明 Book2，类型为 Book */
-       
+        Books Book2 = new Books("aa","bb","cc",6666); /* 声明 Book2，类型为 Book */
+        ThreeKing tkBook = new ThreeKing("aa", "bb", "cc", 6666);
         /* 打印 Book 信息 */
         Book1.display();
-     
+        Book2.display();
+        tkBook.display();
+
 
         /* book 1 详述 */
         Book1.getValues("C Programming",
